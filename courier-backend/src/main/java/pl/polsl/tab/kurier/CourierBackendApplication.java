910f5665f4fe7d74b26cd68ce2784b3693e611dd
Tomasz Skrzyp <Tomasz Skrzyp@DESKTOP-AMBO86B>
2026-04-20 18:48:09 +0200

    feat: introduced backend!!

---
+++ courier-backend/src/main/java/pl/polsl/tab/kurier/CourierBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.tab.kurier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierBackendApplication.class, args);
+	}
+
+}
---
+++ courier-backend/src/test/java/pl/polsl/tab/kurier/CourierBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.tab.kurier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
